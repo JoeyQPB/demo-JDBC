2b3c5e2cb4ebb5bf8bdc80008c252a73801c2341
joey <joey_qpb@hotmail.com>
2023-11-02 19:12:43 +1100

    feat: Department Entity Class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,14 @@
 package application;
 
-import java.sql.Connection;
-
-import db.DB;
+import model.entities.Department;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Connection conn = DB.getConnection();
-		DB.CloseConnection();
+		Department obj = new Department(1, "Books");
+		System.out.println(obj);
+
 	}
 
 }
