9040d3a8a73d83ff21b7dec24de150155cd80c9f
joey <joey_qpb@hotmail.com>
2023-11-04 07:32:19 +1100

    fead: findByDepartment Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,11 @@
 package application;
 
+import java.util.List;
+
 import db.DB;
 import model.dao.DaoFactory;
 import model.dao.SellerDAO;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -15,6 +18,11 @@ public class Program {
 		System.out.println("=== TEST 1: Seller findById ===");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
+
+		System.out.println();
+		System.out.println("=== TEST 2: Seller findByDepartment ===");
+		List<Seller> sellerListByDepartment = sellerDao.findByDepartment(new Department(2, null));
+		sellerListByDepartment.forEach(System.out::println);
 		System.out.println();
 
 		DB.CloseConnection();
