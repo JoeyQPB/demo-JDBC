b81b637b698870f945bf266951793843100d22de
joey <joey_qpb@hotmail.com>
2023-11-03 19:10:55 +1100

    feat: findByID Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,24 +1,16 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDAO;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
-
-		Seller seller = new Seller(21, "jhon doe", "mail", new Date(), 3000.0, obj);
-		System.out.println(seller);
-
 		SellerDAO sellerDao = DaoFactory.createSellerDao();
-		System.out.println(sellerDao);
+		Seller seller = sellerDao.findById(3);
+		System.out.println(seller);
 
 	}
 
